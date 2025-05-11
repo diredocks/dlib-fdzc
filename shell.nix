8c5f1985e6276cbe8bf2352f6c1fc018cc34a749
diredocks <26994007+diredocks@users.noreply.github.com>
2025-05-11 14:09:24 +0800

    git: ignore models

---
+++ shell.nix
@@ -0,0 +1,18 @@
+# shell.nix
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pkg-config
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+    cmake
+    pkg-config
+    gcc
+    opencv
+  ];
+}
